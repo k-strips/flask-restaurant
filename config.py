1feed5f4ad4a599eed00226b14ab7d59f44ca061
k-strips <henblackstep@gmail.com>
2022-01-17 18:12:36 +0000

    init setup

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import environ, path
+from plistlib import load
+from dotenv import load_dotenv
+
+base_dir = os.abspath(os.path.dirname(__file__))
+load_dotenv(path.join(base_dir, '.env'))
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
