16bb3390f0d6d56fc4b07ff824b743d6b6034993
stephen <henblackstep@gmail.com>
2022-03-01 10:25:34 +0000

    [ADD] restaurants routes init

--- restaurant_backend/api/routes.py
+++ restaurant_backend/api/routes.py
@@ -1,10 +1,40 @@
 from flask import current_app as app
 from flask import Blueprint
-from ..services.restaurant_service import create_restaurant
+from ..services.restaurant_service import create_restaurant, fetch_all_restaurants, get_restaurant_by_id, update_restaurant_by_id, update_restaurants, delete_restaurant_by_id, delete_restaurants
 
 # blueprint configuration
 api_bp = Blueprint("api", __name__)
 
+@api_bp.route('/api/v1/restuarants/add', methods=['POST'])
+def add_restaurant(restaurant_data):
+    return create_restaurant(restaurant_data)
+
+
+@api_bp.route('/api/v1/restuarants/update/<int:restuarant_id>', methods=['PUT'])
+def update_restaurant_by_id(restaurant_data):
+    return update_restaurant_by_id(restaurant_data)
+
+
+@api_bp.route('/api/v1/restuarants/update/', methods=['PUT'])
+def update_restaurants(restaurants_data):
+    return update_restaurants(restaurants_data)
+
+
 @api_bp.route('/api/v1/restuarants', methods=['GET'])
-def get_restuarants():
-    return "hello world"
+def fetch_all_restuarants():
+    return fetch_all_restaurants()
+
+
+@api_bp.route('/api/v1/restuarants/<int:restuarant_id>', methods=['GET'])
+def get_restaurant(restuarant_id):
+    return get_restaurant_by_id(restuarant_id)
+
+
+@api_bp.route('/api/v1/restuarants/delete/<int:restuarant_id>', methods=['DELETE'])
+def delete_restuarant_by_id(restuarant_id):
+    return delete_restuarant_by_id(restuarant_id)
+
+
+@api_bp.route('/api/v1/restuarants/delete', methods=['DELETE'])
+def delete_restaurants():
+    return delete_restaurants()
--- restaurant_backend/services/restaurant_service.py
+++ restaurant_backend/services/restaurant_service.py
@@ -2,41 +2,48 @@ from ..models import Restaurant
 
 def create_restaurant(restaurant_data):
     try:
-        return "restuarant created"
+        return f"restuarant created"
     except Exception as e:
         return f'sorry, restuarant creation failed'
 
 
-def update_restaurant(restaurant_id):
+def update_restaurant_by_id(restaurant_id):
     try:
-        return "restuarant updated"
+        return f"restuarant with id {restaurant_id} updated"
     except Exception as e:
         return f'sorry, restuarant creation failed'
 
 
-def list_all_restaurants():
+def update_restaurants(restaurants_data):
+    try:
+        return f"restuarants updated"
+    except Exception as e:
+        return f'sorry, restuarants update failed'
+
+
+def fetch_all_restaurants():
     try:
         return "all restuarants ready"
     except Exception as e:
         return f'sorry, all restuarants failed'
 
 
-def get_one_restaurant(restaurant_id):
+def get_restaurant_by_id(restaurant_id):
     try:
-        return "restuarant created"
+        return f"restuarant with id {restaurant_id} ready"
     except Exception as e:
         return f'sorry, restuarant creation failed'
 
 
-def delete_all_restaurants():
+def delete_restaurants():
     try:
         return "all restuarants deleted"
     except Exception as e:
         return f'sorry, restuarant creation failed'
 
 
-def delete_one_restaurant(restaurant_id):
+def delete_restaurant_by_id(restaurant_id):
     try:
-        return "restuarant deleted"
+        return f"restuarant with id {restaurant_id} deleted"
     except Exception as e:
         return f'sorry, restuarant creation failed'
